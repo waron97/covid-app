7014e68aaa64ddc31a00b4b643e4fc1515ac46bc
Aron <winkler.aron5@gmail.com>
2024-12-14 18:16:17 +0100

    (back-end) Update data export task - Update SQL query to match new tables - Prevent directory creation from crashing application

--- src/back-end/flaskr/tasks.py
+++ src/back-end/flaskr/tasks.py
@@ -11,7 +11,11 @@ def export_state_date_xlsx(date: datetime.datetime):
     fname = f"{datestr}.xlsx"
     save_path = f"static/exports/{fname}"
     serve_path = f"{os.environ.get('API_URL')}/static/exports/{fname}"
-    os.makedirs("static/exports")
+    
+    try:
+        os.makedirs("static/exports", exist_ok=True)
+    except FileExistsError:
+        pass
 
     if os.path.exists(save_path):
         return serve_path
@@ -20,12 +24,14 @@ def export_state_date_xlsx(date: datetime.datetime):
     ws = wb.active
     command = """
         SELECT
-            region_name,
-            SUM(case_total)
-        FROM state_data
-        WHERE date = %s
-        GROUP BY region_name
-        ORDER BY region_name ASC
+            R.region_name,
+            SUM(C.case_total)
+        FROM covid_case_records AS C
+        FULL JOIN states AS S ON C.state_code = S.state_code
+        FULL JOIN regions AS R on S.region_code = R.region_code
+        WHERE C.date = %s
+        GROUP BY R.region_name
+        ORDER BY R.region_name ASC
     """
     with conn.cursor() as c:
         c.execute(command, (datestr,))
